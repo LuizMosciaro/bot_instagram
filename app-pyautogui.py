71b9381901dddf0d422beb8b8a4639b3c13ad4b3
LuizMosciaro <edu.mosciaro@live.com>
2022-05-22 19:38:43 -0400

    Atualização de novo tipo de bot de likes, bem simples

--- app-pyautogui.py
+++ app-pyautogui.py
@@ -1,9 +1,8 @@
 import random
 import pyautogui as p
 from time import sleep as s
-import keyboard
 
-s(2)
+s(3)
 p.hotkey('win','r')
 p.write('https://www.instagram.com/explore/')
 p.press('enter')
@@ -11,30 +10,34 @@ s(8)
 
 #click na primeira foto o bloco
 p.click(365,180)
-s(random.randrange(3,5))
+s(random.randrange(4,6))
 
-
-x = 0
+y = 0
 while True:
+    x = 0
+    while True:
+        #procurando o botao de like
+        like = p.locateOnScreen('coracao.png', confidence=0.7,grayscale=True)
+        p.click(like)
+        s(random.randrange(4,7))
+        #procurando o botao de proximo
+        p.press('right')
+        s(random.randrange(4,7))
 
-    #procurando o botao de like
-    like = p.locateOnScreen('coracao.png', confidence=0.7,grayscale=True)
-    p.click(like)
-    s(random.randrange(2,4))
-    #procurando o botao de proximo
-    p.press('right')
-    s(random.randrange(2,4))
-
-    #depois de 12 curtidas eles tem q carregar a pagina
-    if x >= 12:
-        p.press('esc')
-        s(random.randrange(2,4))
-        p.press('end')
-        s(random.randrange(3,5))
-        p.click(365,180)
-        s(random.randrange(2,4))
-        x = x-12
-    else:
-        pass
-
-    x+=1
+        #depois de 12 curtidas eles tem q carregar a pagina
+        if x >= 12:
+            p.press('esc')
+            s(random.randrange(4,7))
+            p.press('end')
+            p.press('end')
+            s(random.randrange(4,7))
+            p.click(365,180)
+            s(random.randrange(4,7))
+            x = x-12
+            
+        x+=1
+        if y >= 100:
+            p.press('f5')
+            s(random.randrange(4,7))
+        y+=1
+        print(f"Bot deu {y} likes")
